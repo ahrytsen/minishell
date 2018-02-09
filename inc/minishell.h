a1b887e714974fe5fc66e7ff8a939c4be13cd76b
Artur HRYTSENKO <ahrytsen@e1r4p14.unit.ua>
2018-02-09 20:54:33 +0200

    start

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/02 17:32:57 by ahrytsen          #+#    #+#             */
-/*   Updated: 2018/02/02 17:33:54 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2018/02/09 16:20:50 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define MINISHELL_H
 
 # include <libft.h>
+# include <sys/param.h>
+# include <signal.h>
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -6,13 +6,42 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/02 11:02:52 by ahrytsen          #+#    #+#             */
-/*   Updated: 2018/02/02 17:35:13 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2018/02/09 20:54:10 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+void	ft_cd(char *path)
+{
+	chdir(path);
+}
+
 int		main(void)
 {
-	ft_printf("lol\n");
+	char	test[MAXPATHLEN];
+	char	*line;
+
+	while(1)
+	{
+		ft_printf("%s $>", getcwd(test, MAXPATHLEN));
+		ft_bzero(test, MAXPATHLEqN);
+		get_next_line(1, &line);
+	}
 }
+
+/* void sig_handler(int signo) */
+/* { */
+/*   if (signo == SIGINT) */
+/*     ft_printf("received SIGINT\n"); */
+/* } */
+
+/* int main(void) */
+/* { */
+/*   if (signal(SIGINT, sig_handler) == SIG_ERR) */
+/*   ft_printf("\ncan't catch SIGINT\n"); */
+/*   // A long long wait so that we can easily issue a signal to this process */
+/*   while(1) */
+/*     sleep(1); */
+/*   return 0; */
+/* } */
